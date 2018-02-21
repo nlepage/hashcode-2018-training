de804130ad7184c1d9b73a7db0e6e377573d36e5
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-02-21 22:05:28 +0100

    :bug:

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require('fs')
 
-const buffer = readFileSync('./small.in').toString()
+const buffer = readFileSync('./' + process.argv[2] + '.in').toString()
 const [numbers, ...pizza] = buffer.split('\n')
 const [R, C, L, H] = numbers.split(' ')
 
@@ -13,14 +13,16 @@ if (isSliceValid(0, 0, 2, L)) {
   slices.push([0, 0, 2, L])
 }
 
-if (L != 2 && isSliceValid(0, 0, L, 2)) {
-  slices.push([0, 0, L, 2])
-}
+console.log(slices.length)
+
+slices.forEach(slice => {
+  console.log(slice.join(' '))
+})
 
 function isSliceValid(r1, c1, r2, c2) {
   const ingredients = [0, 0]
   for(let r = r1; r <= r2; r++)
-    for(let c = c1; r <= c2; c++) {
+    for(let c = c1; c <= c2; c++) {
       if (matrix[r][c] === 'M') {
         ingredients[0]++
       } else {
---
+++ solve.sh
@@ -0,0 +1,4 @@
+for i in example small medium big
+do
+node parser.js $i > $i.txt
+done
